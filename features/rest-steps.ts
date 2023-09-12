bc3634661daf04224d46e189bfeb95c9f2ba7f52
Markus Tacker <m@coderbyheart.com>
2023-09-12 12:14:20 +0200

    fix: update to bdd-markdown v6

--- features/rest-steps.ts
+++ features/rest-steps.ts
@@ -1,10 +1,7 @@
 import {
 	codeBlockOrThrow,
-	matchGroups,
-	noMatch,
-	type StepRunResult,
+	regExpMatchedStep,
 	type StepRunner,
-	type StepRunnerArgs,
 } from '@nordicsemiconductor/bdd-markdown'
 import { Type } from '@sinclair/typebox'
 import assert from 'assert/strict'
@@ -14,15 +11,11 @@ import { parseRequest } from './parseRequest.js'
 export const steps = (): StepRunner<World>[] => {
 	let res: Response | undefined = undefined
 	return [
-		async ({
-			step,
-			log: {
-				step: { progress },
-			},
-			context,
-		}: StepRunnerArgs<World>): Promise<StepRunResult> => {
-			const match = matchGroups(
-				Type.Object({
+		regExpMatchedStep(
+			{
+				regExp:
+					/^I (?<method>(GET|POST|PUT|DELETE)) (?:to )?`(?<endpoint>https?:\/\/[^/]+)(?<resource>\/[^`]*)`(?<hasBody> with)?$/,
+				schema: Type.Object({
 					method: Type.Union([
 						Type.Literal('GET'),
 						Type.Literal('POST'),
@@ -33,92 +26,90 @@ export const steps = (): StepRunner<World>[] => {
 					resource: Type.RegEx(/^\/.*/),
 					hasBody: Type.Optional(Type.Literal(' with')),
 				}),
-			)(
-				/^I (?<method>(GET|POST|PUT|DELETE)) (?:to )?`(?<endpoint>https?:\/\/[^/]+)(?<resource>\/[^`]*)`(?<hasBody> with)?$/,
-				step.title,
-			)
-			if (match === null) return noMatch
-			const url = new URL(match.resource, match.endpoint).toString()
-			const method = match.method ?? 'GET'
-			progress(`${method} ${url}`)
-			let body = undefined
-			let headers = undefined
-			if (match.hasBody !== undefined) {
-				const parsed = parseRequest(codeBlockOrThrow(step).code)
-				for (const [k, v] of Object.entries(parsed.headers)) {
-					progress(`> ${k}: ${v}`)
+			},
+			async ({ match, step, log: { progress }, context }) => {
+				const url = new URL(match.resource, match.endpoint).toString()
+				const method = match.method ?? 'GET'
+				progress(`${method} ${url}`)
+				let body = undefined
+				let headers = undefined
+				if (match.hasBody !== undefined) {
+					const parsed = parseRequest(codeBlockOrThrow(step).code)
+					for (const [k, v] of Object.entries(parsed.headers)) {
+						progress(`> ${k}: ${v}`)
+					}
+					progress(`> ${parsed.body}`)
+					body = parsed.body
+					headers = parsed.headers
 				}
-				progress(`> ${parsed.body}`)
-				body = parsed.body
-				headers = parsed.headers
-			}
 
-			res = await fetch(url, {
-				method,
-				body: ['POST', 'PUT'].includes(method) ? body : undefined,
-				headers,
-				redirect: 'manual',
-			})
+				res = await fetch(url, {
+					method,
+					body: ['POST', 'PUT'].includes(method) ? body : undefined,
+					headers,
+					redirect: 'manual',
+				})
 
-			progress(`${res.status} ${res.statusText}`)
-			let resBody: string | undefined = undefined
-			if (parseInt(res.headers.get('content-length') ?? '0', 10) > 0) {
-				resBody = await res.text()
-				progress(`< ${resBody}`)
-			}
-			progress(`x-amzn-trace-id: ${res.headers.get('x-amzn-trace-id')}`)
-			context.response = { body: resBody ?? '', headers: res.headers }
-		},
-		async ({ step }: StepRunnerArgs<World>): Promise<StepRunResult> => {
-			const match = matchGroups(
-				Type.Object({
-					code: Type.RegEx(/^[0-9]+$/),
+				progress(`${res.status} ${res.statusText}`)
+				let resBody: string | undefined = undefined
+				if (parseInt(res.headers.get('content-length') ?? '0', 10) > 0) {
+					resBody = await res.text()
+					progress(`< ${resBody}`)
+				}
+				progress(`x-amzn-trace-id: ${res.headers.get('x-amzn-trace-id')}`)
+				context.response = { body: resBody ?? '', headers: res.headers }
+			},
+		),
+		regExpMatchedStep(
+			{
+				regExp: /^the response status code should be `(?<code>[0-9]+)`$/,
+				schema: Type.Object({
+					code: Type.Integer(),
 				}),
-			)(/^the response status code should be `(?<code>[0-9]+)`$/, step.title)
-			if (match === null) return noMatch
-
-			assert.equal(res?.status, parseInt(match.code, 10))
-		},
-		async ({
-			step,
-			context,
-		}: StepRunnerArgs<World>): Promise<StepRunResult> => {
-			const match = matchGroups(
-				Type.Object({
+				converters: {
+					code: (s) => parseInt(s, 10),
+				},
+			},
+			async ({ match }) => {
+				assert.equal(res?.status, match.code)
+			},
+		),
+		regExpMatchedStep(
+			{
+				regExp:
+					/^the response `(?<header>[^`]+)` header should equal `(?<expected>[^`]+)`$/,
+				schema: Type.Object({
 					header: Type.String(),
 					expected: Type.String(),
 				}),
-			)(
-				/^the response `(?<header>[^`]+)` header should equal `(?<expected>[^`]+)`$/,
-				step.title,
-			)
-			if (match === null) return noMatch
-
-			assert.equal(context.response?.headers.get(match.header), match.expected)
-		},
-		async ({
-			step,
-			context,
-		}: StepRunnerArgs<World>): Promise<StepRunResult> => {
-			const match = matchGroups(
-				Type.Object({
+			},
+			async ({ match, context }) => {
+				assert.equal(
+					context.response?.headers.get(match.header),
+					match.expected,
+				)
+			},
+		),
+		regExpMatchedStep(
+			{
+				regExp:
+					/^the response body should be a string matching `(?<regexp>[^`]+)`$/,
+				schema: Type.Object({
 					regexp: Type.String(),
 				}),
-			)(
-				/^the response body should be a string matching `(?<regexp>[^`]+)`$/,
-				step.title,
-			)
-			if (match === null) return noMatch
-
-			assert.match(context.response?.body ?? '', new RegExp(match.regexp, 'i'))
-		},
-		async ({
-			step,
-			context,
-		}: StepRunnerArgs<World>): Promise<StepRunResult> => {
-			if (!/^the response body should equal$/.test(step.title)) return noMatch
-
-			assert.equal(context.response?.body ?? '', codeBlockOrThrow(step).code)
+			},
+			async ({ match, context }) => {
+				assert.match(
+					context.response?.body ?? '',
+					new RegExp(match.regexp, 'i'),
+				)
+			},
+		),
+		{
+			match: (title) => /^the response body should equal$/.test(title),
+			run: async ({ step, context }) => {
+				assert.equal(context.response?.body ?? '', codeBlockOrThrow(step).code)
+			},
 		},
 	]
 }
