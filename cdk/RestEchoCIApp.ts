1232e75ac41133e6d67d2dad367e6890fef58f62
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-05-25 23:34:10 +0200

    style: fix eslint errors

--- cdk/RestEchoCIApp.ts
+++ cdk/RestEchoCIApp.ts
@@ -1,6 +1,6 @@
 import { App } from 'aws-cdk-lib'
 import { RestEchoCIStack } from './stacks/RestEchoCIStack.js'
-import { RestEchoStack } from './stacks/RestEchoStack.js'
+import type { RestEchoStack } from './stacks/RestEchoStack.js'
 
 export class RestEchoCIApp extends App {
 	public constructor(args: ConstructorParameters<typeof RestEchoStack>[1]) {
